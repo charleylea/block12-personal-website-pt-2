d419b3613c7e5c79dfdcae8209f0b41bc52d3ad5
Charley Lea <charleylea@att.net>
2024-12-09 22:14:30 -0500

    Add files via upload
    I will update this again in the future.  Still not getting success page after send message.

---
+++ script03.js
@@ -0,0 +1,6 @@
+function simulateButtonClick() {
+  document.getElementById("submit").click(); // This will trigger the click event on the submit button
+}
+
+// Automatically trigger the button click after 5 seconds
+setTimeout(simulateButtonClick, 5000);
